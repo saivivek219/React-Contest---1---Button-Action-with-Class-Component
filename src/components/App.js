e1a703747d1e28f72eb303a1a4b1be08394ff1b6
saivivek219 <55201381+saivivek219@users.noreply.github.com>
2020-10-21 21:43:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  state = {
+    check: false
+  };
+
+  handleClick = () => {
+    this.setState({
+      check: true
+    });
+  };
+
+  render() {
+    return (
+      <div id="main">
+        <button onClick={this.handleClick}>Click</button>
+        {this.state.check && (
+          <p id="para">
+            "Hello, I've learnt to use the full-stack evaluation tool. This
+            makes me so happy"
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
