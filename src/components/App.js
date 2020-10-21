15f0dc9de1cde52d82839e0eec310ba9d03440b4
saivivek219 <55201381+saivivek219@users.noreply.github.com>
2020-10-21 23:01:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,27 @@ import "../styles/App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isShow: false
+    };
+    this.createText = this.createText.bind(this);
   }
 
-  state = {
-    check: false
-  };
-
-  handleClick = () => {
-    this.setState({
-      check: true
-    });
-  };
+  createText() {
+    this.setState({ isShow: true });
+  }
 
   render() {
     return (
       <div id="main">
-        <button onClick={this.handleClick}>Click</button>
-        {this.state.check && (
+        <button id="click" onClick={this.createText}></button>
+        {this.state.isShow && (
           <p id="para">
-            "Hello, I've learnt to use the full-stack evaluation tool. This
-            makes me so happy"
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
           </p>
         )}
+        {/* Do not remove this main div!! */}
       </div>
     );
   }
